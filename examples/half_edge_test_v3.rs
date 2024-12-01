4e61c2462b2237502b724da0011a609f45d9c482
KobayashiRui <roboroborx782@gmail.com>
2024-12-02 02:27:31 +0900

    update

---
+++ examples/half_edge_test_v3.rs
@@ -0,0 +1,44 @@
+use rgeom::{self, HalfEdgeError, HalfEdgeResult, Vertex};
+
+
+fn main(){
+    let mut mesh =  rgeom::HalfEdgeDS::new();
+
+    // 頂点の追加
+    let v0 = mesh.add_vertex([0.0, 0.0, 0.0]);
+    println!("add {}", v0);
+    let v1 = mesh.add_vertex([1.0, 0.0, 0.0]);
+    let v2 = mesh.add_vertex([1.0, 1.0, 0.0]);
+    let v3 = mesh.add_vertex([0.0, 1.0, 0.0]);
+
+    // 面の追加
+    mesh.add_face(&[v0, v1, v2]).unwrap_or_else(|e| panic!("{}", e));
+    mesh.add_face(&[v0, v2, v3]).unwrap_or_else(|e| panic!("{}", e));
+
+    // ハーフエッジIDの取得
+    let he_id = mesh
+        .faces
+        .get(&0)
+        .and_then(|face| face.edge)
+        .unwrap_or_else(|| panic!("Face 0 does not have an edge"));
+
+    // エッジナビゲータを使用したチェーンアクセス
+    let navigator = rgeom::HalfEdgeHandler::new(&mesh, he_id);
+
+    let vertex = (|| -> Result<&Vertex, HalfEdgeError> {
+        let vertex = navigator.prev()?.twin()?.vertex()?;
+        Ok(vertex)
+    })();
+
+    match vertex {
+        Ok(vertex) => {
+            // 成功時の処理
+            println!("Vertex obtained: {:?}", vertex);
+        }
+        Err(e) => {
+            // エラー時の処理
+            eprintln!("Failed to obtain vertex: {}", e);
+        }
+    }
+
+}
--- src/mesh_slice_v2.rs
+++ src/mesh_slice_v2.rs
@@ -1,11 +1,24 @@
-use crate::{HalfEdgeHandler, HalfEdgeID, HalfEdgeDS};
+use crate::{HalfEdgeHandler, HalfEdgeID, HalfEdgeDS, Vertex, HalfEdgeError};
 use crate::Plane;
 
-pub fn slice(he_ds: &HalfEdgeDS, plan: &Plane){
+fn find_intersectiong_edges(he_ds: &HalfEdgeDS, plane:&Plane){
     let mut intersection_he: Vec::<HalfEdgeID> = Vec::new();
     for (he_id, he) in &he_ds.half_edges {
         let he_nav = HalfEdgeHandler::new(&he_ds, he_id.clone());
-        let end_vertex = he_nav.vertex();
-        let start_vertex = he_nav.prev().vertex();
+        let edge = (|| -> Result<(&Vertex, &Vertex), HalfEdgeError> {
+            let end_vertex = he_nav.vertex()?;
+            let start_vertex = he_nav.prev()?.vertex()?;
+            Ok((start_vertex, end_vertex))
+        })();
+
+        //エラー処理
+        if let Err(_) = edge {
+            continue;
+        }
     }
+}
+
+
+pub fn slice(he_ds: &HalfEdgeDS, plane: &Plane){
+    find_intersectiong_edges(he_ds, plane);
 }
